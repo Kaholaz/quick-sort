ea57a3c85c3c75f428a1b95c94aa6641b665f9e3
Kaholaz <nim.nimern@gmail.com>
2022-09-08 22:04:39 +0200

    Add arunan-sort

--- øving.py
+++ øving.py
@@ -14,6 +14,18 @@ def quick_sort_inner(a: list, start: int, stop: int) -> None:
     quick_sort_inner(a, start, piv - 1)
     quick_sort_inner(a, piv + 1, stop)   
 
+def arunan_sort(a: list) -> list:
+    def inner(a: list, start, stop):
+        if stop - start <= 2:
+            median_three_sort(a, start, stop)
+            return
+    
+        piv = partition_arunan(a, start, stop)
+        inner(a, start, piv - 1)
+        inner(a, piv + 1, stop)   
+    inner(a, 0, len(a) - 1)
+    return a
+
 
 def median_three_sort(a: list, start: int, stop: int) -> int:
     mid = (start + stop) // 2
@@ -38,17 +50,35 @@ def partition(a: list, start: int, stop: int) -> int:
     left = start + 1
     right = stop - 2
     while True:
+        # Skip values that dont need to swap
         while a[right] > middle_value: right -= 1
         while a[left] < middle_value: left += 1
 
         if right <= left: break
+        
+        # Swap values and increment
         a[left], a[right] = a[right], a[left]
-
         right, left = right - 1, left + 1
     
     # Fix piv to be in the middle
     a[piv], a[left] = a[left], a[piv]
     return left
+
+def partition_arunan(nums, low, high):
+    m = median_three_sort(nums, low, high)
+    nums[m], nums[high - 1] = nums[high - 1], nums[m]
+    pivot = nums[high - 1]
+
+    min_index = low + 1
+
+    for current_index in range(min_index, high - 1):
+        if nums[current_index] <= pivot:
+            nums[min_index], nums[current_index] = nums[current_index], nums[min_index]
+            min_index += 1
+
+    nums[min_index], nums[high - 1] = nums[high - 1], nums[min_index]
+
+    return min_index
 
 
 def quick_sort_dual(a: list) -> list:
@@ -72,18 +102,23 @@ def median_four_sort(a: list, start: int, stop: int) -> tuple[int, int]:
     mid_1 = start + (stop - start) // 3
     mid_2 = start + (2 * (stop - start)) // 3
 
+    # Arrange outer
     if a[start] > a[stop]:
         a[start], a[stop] = a[stop], a[start]
     
+    # Arrange inner
     if a[mid_1] > a[mid_2]:
         a[mid_1], a[mid_2] = a[mid_2], a[mid_1]
 
+    # Arrange top
     if a[mid_2] > a[stop]:
         a[mid_2], a[stop] = a[stop], a[mid_2]
     
+    # Arrange bottom
     if a[start] > a[mid_1]:
         a[start], a[mid_1] = a[mid_1], a[start]
 
+    # Arrange inner
     if a[mid_1] > a[mid_2]:
         a[mid_1], a[mid_2] = a[mid_2], a[mid_1]
 
@@ -97,7 +132,7 @@ def partition_dual(a: list, start: int, stop: int) -> tuple[int, int]:
     a[piv_1], a[start + 1] = a[start + 1], a[piv_1]
     a[piv_2], a[stop - 1] = a[stop - 1], a[piv_2]
     
-    # Update pivot indecies and retrive value
+    # Update pivot indecies and retrive values
     piv_1, piv_2 = start + 1, stop - 1
     value_1, value_2 = a[piv_1], a[piv_2]
 
@@ -145,9 +180,9 @@ def quick_sort_insert_20(a: list):
     inner(a, 0, len(a) - 1)
     return a
 
-def quick_sort_insert_40(a: list):
+def quick_sort_insert_10(a: list):
     def inner(a: list, start: int, stop: int):
-        if stop - start <= 40:
+        if stop - start <= 10:
             insertion_sort(a, start, stop)
             return
     
@@ -162,14 +197,14 @@ def quick_sort_insert_40(a: list):
 def insertion_sort(a: list, start: int, end: int) -> list:
     for i in range(start + 1, end + 1):
         for j in range(i, start, -1):
-            if a[j-1] < a[j]: break
+            if a[j-1] <= a[j]: break
             a[j-1], a[j] = a[j], a[j-1]
 
     return a
 
-sorting_algs = [sorted, quick_sort, quick_sort_dual, quick_sort_insert_20, quick_sort_insert_40]
+sorting_algs = [sorted, quick_sort, quick_sort_dual, quick_sort_insert_20, arunan_sort]
 def test_sort_sorted():
-    a = [i for i in range(10)]
+    a = [i for i in range(100)]
     
     results = [alg(a[:]) for alg in sorting_algs]
 
@@ -177,7 +212,7 @@ def test_sort_sorted():
     assert all(answer == result for result in results)
 
 def test_sort_inverted():
-    a = [i for i in range(50, -1, -1)]
+    a = [-i for i in range(100)]
 
     results = [alg(a[:]) for alg in sorting_algs]
 
@@ -212,7 +247,7 @@ if __name__=="__main__":
 
     timeings = {alg: list() for alg in sorting_algs}
     for n in tqdm(ns):
-        original_nums = [random() for _ in range(n)]
+        original_nums = [random() for n in range(n)]
 
         for func in timeings.keys():
             timeings[func].append(timeit("func(nums)", setup="nums=original_nums[:]", number=1, globals=vars()))
